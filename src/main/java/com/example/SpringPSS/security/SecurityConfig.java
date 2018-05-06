5d6492211d5971d3c5478199955dd9d66abbbb7e
TadzikT <znubionek@gmail.com>
2018-05-06 20:23:06 +0200

    registering now works

--- src/main/java/com/example/SpringPSS/security/SecurityConfig.java
+++ src/main/java/com/example/SpringPSS/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/css/**", "/index", "/register").permitAll()
                 .antMatchers("/user/**").hasAuthority("READ_PRIVILEGE")
